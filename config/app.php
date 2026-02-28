52e77d73740c5b37911bed71800460e288413c32
denisphamzero7 <pnhau2001@gmail.com>
2026-02-28 08:22:32 +0700

    khoi tao module

--- config/app.php
+++ config/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Facade;
+use Modules\ModuleServiceProvider;
 
 return [
 
@@ -176,6 +177,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        ModuleServiceProvider::class,
 
     ],
 
---
+++ modules/ModuleServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+namespace Modules;
+use Illuminate\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider{
+    public function boot()
+    {
+
+    }
+ public function register()
+    {
+
+    }
+}
