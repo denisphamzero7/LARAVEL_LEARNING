59b7ff42203ab314fa9dd734fe026a3a853f02ff
denisphamzero7 <pnhau2001@gmail.com>
2026-03-01 11:48:51 +0700

    update provider

--- modules/ModuleServiceProvider.php
+++ modules/ModuleServiceProvider.php
@@ -1,10 +1,21 @@
 <?php
 namespace Modules;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\File;
 
 class ModuleServiceProvider extends ServiceProvider{
     public function boot()
     {
+         $dir = array_map('basename', File::directories(__DIR__));
+         dd($dir);
+         if(!empty($dir)) {
+             foreach ($dir as $module) {
+                 $routePath = __DIR__ . '/' . $module . '/routes.php';
+                 if (file_exists($routePath)) {
+                     include_once $routePath;
+                 }
+             }
+         }
 
     }
  public function register()
