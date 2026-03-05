3b770f6d985082a236731e8a6bc0a70b24baadd5
denisphamzero7 <pnhau2001@gmail.com>
2026-03-05 09:06:56 +0700

    authentication

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +16,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user = new User();
+        $user->name ='hau';
+        $user->email='pnhau2001@gmail.com';
+        $user->password = Hash::make('12345678');
+        $user->save();
     }
 }
